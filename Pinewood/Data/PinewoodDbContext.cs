8d768380746bdece7bf7c55d948d19331cf723b4
Hareer <hareerk786@gmail.com>
2024-09-24 11:30:59 +0100

    Add project files.

---
+++ Pinewood/Data/PinewoodDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Pinewood.Models;
+
+namespace Pinewood.Data
+{
+    public class PinewoodDbContext : DbContext
+    {
+        public PinewoodDbContext (DbContextOptions<PinewoodDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Pinewood.Models.Customer> Customer { get; set; } = default!;
+    }
+}
---
+++ Pinewood/Models/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata;
+
+namespace Pinewood.Models
+{
+    public class Customer
+    {
+        public Guid Id { get; set; }
+        [Required(ErrorMessage = "You must provide a name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "You must provide an address")]
+        public string Address { get; set; }
+        [Required(ErrorMessage = "You must provide a phone number")]
+        [Display(Name = "Phone Number")]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Not a valid phone number")]
+        public string PhoneNumber { get; set; }
+        [Required(ErrorMessage = "You must provide an email address")]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage = "Invalid email address provided")]
+        public string Email { get; set; }
+
+    }
+}
